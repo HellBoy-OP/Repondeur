e94a291031eeb96ffbd340322e086e6fe9cb3389
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-09-06 19:25:31 +0530

    Let's see if this works

--- userbot/modules/account.py
+++ userbot/modules/account.py
@@ -7,13 +7,16 @@ from userbot.events import *
 async def _(event):
   if event.fwd_from:
         return
-  async with bot.conversation(chat) as conv:
+  chat = event.chat_id
+  async with client.conversation(chat) as conv:
     msg1 = await conv.send_message('Do You Really Want To Delete You Account')
     msg2 = await conv.get_response()
     msg3 = await conv.get_reply()
-    if msg2 or msg3 == "yes": == "yes" or "Yes" or "YES":
-        event.edit("I am deleting my account with own faith no one is responsible except me")
-        await bot(functions.account.DeleteAccountRequest("I am Deleteing my Account on my own risk"))
+    x = msg2 or msg3
+    y = x.lower()
+    if y == "yes":
+      await event.edit("I am deleting my account with own faith no one is responsible except me")
+      await bot(functions.account.DeleteAccountRequest("I am Deleteing my Account on my own risk"))
     else:
       return await event.edit(f"Since You Havnt Replied me `yes` delete your account I am aborting")
 
